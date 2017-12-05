22153657d371b6af073762e10109ddada3a66750
Kunal <kshantilal03@gmail.com>
2017-12-05 20:33:36 +1300

    Added in completed bootstrap slider with custom image crop

--- front-page.php
+++ front-page.php
@@ -1,36 +1,40 @@
 <?php get_header(); ?>
 	<div class="row">
 		<div class="container">
+				<?php  
+				$arg = array(
+					'post_type' => 'slider', 
+					'posts_per_page' => 5				
+				);
+				$images = new WP_Query($arg);
+
+				?>
 			<div class="col-xs-12">
+				<?php if($images->have_posts()): ?>
+					<?php while($images->have_posts()): $images->the_post(); ?>
+
+					<?php endwhile; ?>
+				
+
 				<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
 					<!-- Indicators -->
 					<ol class="carousel-indicators">
-						<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-						<li data-target="#carousel-example-generic" data-slide-to="1"></li>
-						<li data-target="#carousel-example-generic" data-slide-to="2"></li>
-						<li data-target="#carousel-example-generic" data-slide-to="3"></li>
-						<li data-target="#carousel-example-generic" data-slide-to="4"></li>
+						<?php while($images->have_posts()): $images->the_post(); ?>
+							<?php $i = 0; ?>
+							<li data-target="#carousel-example-generic" data-slide-to="<?$i ?>" <?php if($i == 0): ?><?php endif; ?>></li>
+						<?php endwhile; ?>
 					</ol>
-
-					<!-- Wrapper for slides -->
 					<div class="carousel-inner" role="listbox">
-						<?php $slider = get_posts(array(
-							'post_type' => 'slider', 
-							'posts_per_page' => 5
-						)); 
-
-
-						?>
-							<?php $count = 0; ?>
-							<?php foreach($slider as $slide): ?>
-	
-							<div class="item <?php echo ($count == 0) ? 'active' : ''; ?>">
-
-								<img src="<?php echo wp_get_attachment_url(get_post_thumbnail_id($slide->ID)) ?>" class="img-responsive"/>
-							</div>
-							<?php $count++; ?>
-							<?php endforeach; ?>
-					</div>
+						<?php $count = 0 ?>
+						<?php while($images->have_posts()): $images->the_post(); ?>
+							
+								<div class="item <?php echo ($count == 0) ? 'active' : ''; ?>">
+									<?php the_post_thumbnail('Front-page-feature', array('class' => 'img-responsive')); ?>
+								</div>
+						<?php $count++; ?>
+						<?php endwhile; ?>
+					</div>	
+				
 
 					<!-- Controls -->
 					<a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
@@ -41,55 +45,10 @@
 						<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
 						<span class="sr-only">Next</span>
 					</a>
+					<?php endif; ?>
 				</div>
 			</div>
-
-			<?php  
-			$arg = array(
-				'post_type' => 'slider', 
-				'posts_per_page' => 5				
-			);
-			$images = new WP_Query($arg);
-
-			?>
-			<?php if($images->have_posts()): ?>
-				<?php while($images->have_posts()): $images->the_post(); ?>
-
-				<?php endwhile; ?>
-			
-
-			<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-					<!-- Indicators -->					
-					<ol class="carousel-indicators">
-					<?php while($images->have_posts()): $images->the_post(); ?>
-						<?php $i = 0; ?>
-						<li data-target="#carousel-example-generic" data-slide-to="<?$i ?>" <?php if($i == 0): ?> active <?php endif; ?>></li>
-					<?php endwhile; ?>
-					</ol>
-					<?php while($images->have_posts()): $images->the_post(); ?>
-						<?php $count = 0 ?>
-							<div class="item <?php echo ($count == 0) ? 'active' : ''; ?>">
-			
-									<?php the_post_thumbnail('Front-page-feature', array('class' => 'img-responsive')); ?>
-
-							</div>
-					<?php endwhile; ?>	
-					</div>
-
-					<!-- Controls -->
-					<a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-						<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-						<span class="sr-only">Previous</span>
-					</a>
-					<a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-						<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-						<span class="sr-only">Next</span>
-					</a>
-				</div>
-			<?php endif; ?>
-
-
-		</div>
+		</div>		
 	</div>
 <?php get_footer(); ?>
 
