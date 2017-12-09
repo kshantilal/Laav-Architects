13d3221de7da330a67d52d4db04458ce34f1bfa7
Kunal <kshantilal03@gmail.com>
2017-12-09 23:40:27 +1300

    Added in functionality to add a different image to a post

--- single.php
+++ single.php
@@ -1,19 +1,23 @@
 <?php get_header(); ?>
 	<div class="container">
 		<div class="row">
+
+ 			 
 			<?php if(have_posts()): ?>
 				<?php while(have_posts()): the_post(); ?>
-
+					
 
 					<div class="col-md-6">
 						<h3>#<?php echo get_post_meta($post->ID, 'ProjectNumber', true); ?></h3>
 						<h2><?php the_title(); ?></h2>
 						<?php the_post_thumbnail('Single-Image'); ?>
 						
-						
-						<?php $customImage = get_post_meta($post->ID, 'custom_image_data', true); 
-							var_dump($customImage);
-						?>
+						<?php $custom = get_post_meta($post->ID, 'custom_image_data', true); ?>
+						<?php $customImage = $custom['src'];?>
+						<?php $ImageCropped = wp_get_attachment_image( $customImage, 'medium' );?>
+
+							<img src="<?php echo $customImage; ?>" alt="test">
+							
 
 					</div>
 					<div class="col-md-6">
